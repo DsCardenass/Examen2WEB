fd32b60ade060848e0833e148dad46635d48bd8c
Dionny Cárdenas <ds.cardenass@uniandes.edu.co>
2024-05-23 10:47:03 -0500

    Inicio de servicio para Propuesta

---
+++ src/propuesta/propuesta.controller.ts
@@ -0,0 +1,8 @@
+import { Controller } from '@nestjs/common';
+import { PropuestaService } from './propuesta.service';
+
+@Controller('propuesta')
+export class PropuestaController {
+
+    constructor(private readonly propuestaService: PropuestaService) {}
+}
--- src/propuesta/propuesta.module.ts
+++ src/propuesta/propuesta.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { PropuestaService } from './propuesta.service';
 import { PropuestaEntity } from './propuesta.entity/propuesta.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PropuestaController } from './propuesta.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([PropuestaEntity])],
-  providers: [PropuestaService]
+  providers: [PropuestaService],
+  controllers: [PropuestaController]
 })
 export class PropuestaModule {}
