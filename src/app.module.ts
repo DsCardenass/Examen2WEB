c20ca1fe56948cb027cbd71175928548a57b2f8c
Dionny Cárdenas <ds.cardenass@uniandes.edu.co>
2024-05-26 16:51:27 -0500

    Actualizacion app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,26 @@ import { ProfesorModule } from './profesor/profesor.module';
 import { EstudianteModule } from './estudiante/estudiante.module';
 import { PropuestaModule } from './propuesta/propuesta.module';
 import { ProyectoModule } from './proyecto/proyecto.module';
+import { EstudianteEntity } from './estudiante/estudiante.entity/estudiante.entity';
+import { ProfesorEntity } from './profesor/profesor.entity/profesor.entity';
+import { PropuestaEntity } from './propuesta/propuesta.entity/propuesta.entity';
+import { ProyectoEntity } from './proyecto/proyecto.entity/proyecto.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ProfesorModule, EstudianteModule, PropuestaModule, ProyectoModule],
+  imports: [ProfesorModule, EstudianteModule, PropuestaModule, ProyectoModule,TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'museum',
+    entities: [EstudianteEntity, ProfesorEntity, PropuestaEntity, ProyectoEntity],
+    dropSchema: true,
+    synchronize: true,
+    keepConnectionAlive: true
+  })
+],
   controllers: [AppController],
   providers: [AppService],
 })
