7f7a05d06e7403bdc45e4a00439285ab3065bf83
Dionny Cárdenas <ds.cardenass@uniandes.edu.co>
2024-05-26 19:08:03 -0500

    Fin del back

--- src/profesor/profesor.module.ts
+++ src/profesor/profesor.module.ts
@@ -3,9 +3,10 @@ import { ProfesorService } from './profesor.service';
 import { ProfesorEntity } from './profesor.entity/profesor.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProfesorController } from './profesor.controller';
+import { PropuestaEntity } from 'src/propuesta/propuesta.entity/propuesta.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProfesorEntity])],
+  imports: [TypeOrmModule.forFeature([ProfesorEntity, PropuestaEntity])],
   providers: [ProfesorService],
   controllers: [ProfesorController]
 })
--- src/propuesta/propuesta.module.ts
+++ src/propuesta/propuesta.module.ts
@@ -3,9 +3,11 @@ import { PropuestaService } from './propuesta.service';
 import { PropuestaEntity } from './propuesta.entity/propuesta.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PropuestaController } from './propuesta.controller';
+import { ProyectoEntity } from 'src/proyecto/proyecto.entity/proyecto.entity';
+import { ProfesorEntity } from 'src/profesor/profesor.entity/profesor.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PropuestaEntity])],
+  imports: [TypeOrmModule.forFeature([PropuestaEntity, ProyectoEntity, ProfesorEntity])],
   providers: [PropuestaService],
   controllers: [PropuestaController]
 })
